cf10270062085a097ee08baa488802b8152705a9
yathindrasj <150610267+yathindrasj@users.noreply.github.com>
2023-11-12 21:55:51 +0100

    Update tests_math_quiz.py

--- tests_math_quiz.py
+++ tests_math_quiz.py
@@ -1,30 +1,39 @@
 import unittest
-from math_quiz import function_A, function_B, function_C
+from math_quiz import limit, math_ops, numericals
 
 
 class TestMathGame(unittest.TestCase):
 
-    def test_function_A(self):
+    def test_function_random(self):
         # Test if random numbers generated are within the specified range
         min_val = 1
         max_val = 10
-        for _ in range(1000):  # Test a large number of random values
-            rand_num = function_A(min_val, max_val)
+        for i in range(1000):  # Test a large number of random values
+            rand_num = limit(min_val, max_val)
             self.assertTrue(min_val <= rand_num <= max_val)
 
-    def test_function_B(self):
+    def test_function_operation(self):
         # TODO
+        listOps=['+' , '-' , '*' , '/' , '%']
+        for i in listOps:
+            available_operation = math_ops(random.choice(['+', '-', '*']))
+            self.assertTrue(i==available_operation)
         pass
 
-    def test_function_C(self):
-            test_cases = [
-                (5, 2, '+', '5 + 2', 7),
-                ''' TODO add more test cases here '''
-            ]
+    def test_function_Calculation(self):
+        test_cases = [
+    (5, 2, '+', '5 + 2', 7),
+    (3, 2, '-', '3 - 2', 1),
+    (6, 5, '*', '6 * 5', 30),
+    (8, 2, '/', '8 / 2', 4)
+                     ]
 
-            for num1, num2, operator, expected_problem, expected_answer in test_cases:
-                # TODO
-                pass
+       for num_1, num_2, operator, expected_problem, expected_answer in test_cases:
+           calculated_problem, calculated_answer = numericals(num_1, num_2, operator)
+           self.assertTrue (calculated_problem == expected_problem)
+           self.assertTrue (calculated_answer == expected_answer)
+           print(f"Test Passed: {calculated_problem} = {calculated_answer}")
 
-if __name__ == "__main__":
+
+if name == "main":
     unittest.main()
