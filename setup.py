abb871f670f410b60887b7e3054df258f7bea3f2
yathindrasj <150610267+yathindrasj@users.noreply.github.com>
2023-11-13 12:29:07 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='math_quiz_game',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        # List any dependencies your project may have
+    ],
+    entry_points={
+        'console_scripts': [
+            'math_quiz_game = math_quiz:main',
+        ],
+    },
+    test_suite='tests',
+)
 
